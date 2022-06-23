93230eca34ca2d15a2402e1922826f070b57c429
mayank2193 <mayankmaurya2193@gmail.com>
2022-06-23 09:09:42 +0000

    modified

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from appium.webdriver import Remote
 from appium_flutter_finder.flutter_finder import FlutterElement, FlutterFinder
@@ -22,22 +23,6 @@ finder = FlutterFinder()
 # text_element = FlutterElement(driver, text_finder)
 # print(text_element.text)
 
-key_finder = finder.by_value_key("next_route_key")
-goto_next_route_element = FlutterElement(driver, key_finder)
-print(goto_next_route_element.text)
-goto_next_route_element.click()
+time.sleep(5)
 
-back_finder = finder.page_back()
-back_element = FlutterElement(driver, back_finder)
-back_element.click()
-
-tooltip_finder = finder.by_tooltip("Increment")
-driver.execute_script('flutter:waitFor', tooltip_finder, 100)
-
-floating_button_element = FlutterElement(driver, tooltip_finder)
-floating_button_element.click()
-
-counter_finder = finder.by_value_key("counter")
-counter_element = FlutterElement(driver, counter_finder)
-print(counter_element.text)
 driver.quit()
